27f74f559a2e2c4dcb917089ffb96784e1a39a7a
DanaKonyk <132483408+DanaKonyk@users.noreply.github.com>
2023-11-23 12:35:14 +0200

    2.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
